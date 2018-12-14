9ce35531118323695303b4ad3766b22f2d3c43d6
Mason Dobbs <masondobbs354@gmail.com>
2018-12-13 22:57:53 -0600

    wins & losses work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,122 @@
 
 
-
 var playerValue = 0;
 
 var computerValue;
 
+var wins = 0;
 
-computerValue = Math.floor(Math.random()*500) +25;
+var loses = 0;
 
+
+computerValue = Math.floor(Math.random()*250) +25;
 
 console.log(computerValue);
 
-$("#ruby").on("click", function(){
 var ruby = Math.floor(Math.random() * 25) +1;
+
+var sapphire = Math.floor(Math.random() * 25) +1;
+
+var emerald = Math.floor(Math.random() * 25) +1;
+
+var diamond = Math.floor(Math.random() * 25) +1;
+
+$("#totalScore").html(playerValue);
+
+$("#wins").html("Wins: " + wins);
+
+$("#losses").html("Losses: " + loses);
+
+$("#ruby").on("click", function(){
+playerValue += ruby; 
 console.log(ruby);
+console.log(playerValue);
+$("#totalScore").html(playerValue);
+
+if(playerValue > computerValue){
+    $("#randomNumber").html("You Lose!");
+    loses++;
+    console.log(loses);
+    $("#losses").html("Losses: " + loses++);
+
+}
+else if(playerValue === computerValue){
+    $("#randomNumber").html("You Win!");
+    wins++;
+    console.log("You Win!");
+    $("#wins").html("Wins: " + wins++);
+}
+
+
 });
 
 $("#sapphire").on("click", function(){
-    var sapphire = Math.floor(Math.random() * 25) +1;
+    playerValue += sapphire;
     console.log(sapphire);
+    console.log(playerValue);
+    $("#totalScore").html(playerValue);
+
+    if(playerValue > computerValue){
+        $("#randomNumber").html("You Lose!");
+        loses++;
+        console.log(loses);
+        $("#losses").html("Losses: " + loses++)
+    }
+    else if(playerValue === computerValue){
+        $("#randomNumber").html("You Win!");
+        wins++;
+        console.log("You Win!");
+        $("#wins").html("Wins: " + wins++);
+    }
+
+
+
 });
 
 $("#emerald").on("click", function(){
-    var emerald = Math.floor(Math.random() * 25) +1;
+    playerValue += emerald;
     console.log(emerald);
+    console.log(playerValue)
+    $("#totalScore").html(playerValue);
+
+    if(playerValue > computerValue){
+        $("#randomNumber").html("You Lose!");
+        loses++;
+        console.log(loses);
+        $("#losses").html("Losses: " + loses++)
+    }
+    else if(playerValue === computerValue){
+        $("#randomNumber").html("You Win!");
+        wins++;
+        console.log("You Win!");
+        $("#wins").html("Wins: " + wins++);
+    }
+
+
 });
 
 $("#diamond").on("click", function(){
-    var diamond = Math.floor(Math.random() * 25) +1;
+    playerValue += diamond
     console.log(diamond);
+    console.log (playerValue)
+    $("#totalScore").html(playerValue);
+
+    if(playerValue > computerValue){
+        $("#randomNumber").html("You Lose!");
+        loses++;
+        console.log(loses);
+        $("#losses").html("Losses: " + loses++)
+    }
+    else if(playerValue === computerValue){
+        $("#randomNumber").html("You Win!");
+        wins++;
+        console.log("You Win!");
+        $("#wins").html("Wins: " + wins++);
+    }
+
+
 });
+
 
 $("#randomNumber").html(computerValue);
 
